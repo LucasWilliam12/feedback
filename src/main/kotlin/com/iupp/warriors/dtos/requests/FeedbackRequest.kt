c35faf2a27e59e22cc6daf415a0fe0294458c779
LucasWilliam12 <lucas.jordao@zup.com.br>
2021-06-10 15:08:48 -0300

    Corrigindo validation das classes de feedback

--- src/main/kotlin/com/iupp/warriors/dtos/requests/FeedbackRequest.kt
+++ src/main/kotlin/com/iupp/warriors/dtos/requests/FeedbackRequest.kt
@@ -4,26 +4,27 @@ import com.iupp.warriors.models.Feedback
 import com.iupp.warriors.repositories.FuncionarioRepository
 import com.iupp.warriors.utils.exceptions.ObjectNotFoundException
 import io.micronaut.core.annotation.Introspected
+import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
 @Introspected
 class FeedbackRequest(
-    @NotNull
-    @NotBlank
+    @field:NotNull
+    @field:NotBlank
     val descricao: String,
-    @NotNull
-    @NotBlank
+    @field:NotNull
+    @field:NotBlank
     val titulo: String,
-    @NotNull
-    @Positive
+    @field:NotNull
+    @field:Positive
     val avaliado: Long,
-    @NotNull
-    @Positive
+    @field:NotNull
+    @field:Positive
     val avaliador: Long
 ) {
-    fun toModel(funcionarioRepository: FuncionarioRepository): Feedback {
+    fun toModel(@Valid funcionarioRepository: FuncionarioRepository): Feedback {
         val avaliado = funcionarioRepository.findById(avaliado).orElseThrow{ ObjectNotFoundException("Avaliado não encontrado pelo id informado") }
         val avaliador = funcionarioRepository.findById(avaliador).orElseThrow{ ObjectNotFoundException("Avaliador não encontrado pelo id informado") }
 
--- src/main/kotlin/com/iupp/warriors/models/Feedback.kt
+++ src/main/kotlin/com/iupp/warriors/models/Feedback.kt
@@ -3,29 +3,30 @@ package com.iupp.warriors.models
 import io.micronaut.core.annotation.Introspected
 import java.time.LocalDateTime
 import javax.persistence.*
+import javax.validation.Valid
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 @Entity
 @Introspected
 class Feedback(
-    @NotNull
-    @NotEmpty
+    @field:NotNull
+    @field:NotEmpty
     var descricao: String,
-    @NotNull
-    @NotEmpty
+    @field:NotNull
+    @field:NotEmpty
     var titulo: String,
-    @NotNull
+    @field:NotNull
     @ManyToOne(optional = false)
     @JoinColumn(name = "avaliado_id")
     var avaliado: Funcionario,
-    @NotNull
+    @field:NotNull
     @ManyToOne(optional = false)
     @JoinColumn(name = "avaliador_id")
     var avaliador: Funcionario
 ) {
 
-    fun atualiza(request: Feedback) {
+    fun atualiza(@Valid request: Feedback) {
         this.titulo = request.titulo
         this.descricao = request.descricao
         this.avaliado = request.avaliado
